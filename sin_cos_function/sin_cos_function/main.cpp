a833647232c1f9f2cb0ecc2688f67f82a161180f
Kim-Sehyeong <david0018@naver.com>
2024-10-30 17:38:20 +0900

    10.30
    10.30

--- sin_cos_function/sin_cos_function/main.cpp
+++ sin_cos_function/sin_cos_function/main.cpp
@@ -6,17 +6,23 @@ const double pi = 3.141592653589793238462643383; //원주율
 double value, rad, value_sin, value_cos = 0.0; //변수 선언
 double get_rad(double num); //함수 선언
 
-
-
 int main(void)
 {
 	printf("Input value of degree : ");
 	scanf_s("%lf", &value); //각도 값 입력 받음
 
-	rad = get_rad(value); //라디안 변환 함수 호출
-	value_sin = sin(rad); //변수 value_sin에 sin값 저장
-	value_cos = cos(rad); //변수 value_cos에 cos값 저장
-	printf("sin(%.2lf) = %.2lf, cos(%.2lf) = %.2lf", value, value_sin, value, value_cos);
+	if ((value >= 0) && (value <= 180))
+	{
+		rad = get_rad(value); //라디안 변환 함수 호출
+		value_sin = sin(rad); //변수 value_sin에 sin값 저장
+		value_cos = cos(rad); //변수 value_cos에 cos값 저장
+		printf("sin(%.2lf) = %.2lf, cos(%.2lf) = %.2lf", value, value_sin, value, value_cos);
+	}
+	else
+	{
+		printf("ERROR");
+	}
+
 
 	return 0;
 }
