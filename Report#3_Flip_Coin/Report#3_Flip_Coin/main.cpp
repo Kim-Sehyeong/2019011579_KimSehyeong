b42f854cdc67394c0aa696aa1860f2ee5e786ae1
Kim-Sehyeong <threebros00@gmail.com>
2024-11-06 17:29:58 +0900

    11.06
    11.06

---
+++ Report#3_Flip_Coin/Report#3_Flip_Coin/main.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define NUM_FLIP 10000
+#define COIN_FWD_BWD 3
+#define COIN_FWD_STATUS 1
+#define COIN_BWD_STATUS 2
+#define COIN_NOT_VALID 0
+#define INITSTATUS 100
+#define END_COND 999
+
+
+void GenRandSeed()
+{
+    srand((unsigned int)(time(NULL)));
+    return;
+}
+
+unsigned int GenRandFlipCoin(unsigned int nRange)
+{
+    unsigned int nRes = 0;
+    unsigned int nNum = rand();
+    while (1)
+    {
+
+        nRes = ((unsigned int)(rand()) % (nRange));
+        if ((nRes == COIN_FWD_STATUS) || (nRes == COIN_BWD_STATUS))
+        break;
+    }
+
+    return nRes;
+}
+
+
+int main(void)
+{
+    GenRandSeed();
+
+    int nNumSumFwdFlip = 0;
+    int nNumSumBwdFlip = 0;
+    double dVarFwdFlip = 0.0;
+    double dVarBwdFlip = 0.0;
+    double dStdevFwdFlip = 0.0;
+    double dStdevBwdFlip = 0.0;
+
+
+    for (unsigned int i = 0; i < NUM_FLIP; i++)
+    {
+
+        //0: fwd, 1: bwd
+        unsigned int nStatusFlipCoin = GenRandFlipCoin(COIN_FWD_BWD);
+        
+
+        switch (nStatusFlipCoin)
+        {
+        case COIN_FWD_STATUS:
+        {
+            nNumSumFwdFlip++;
+            break;
+        }
+        case COIN_BWD_STATUS:
+        {
+            nNumSumBwdFlip++;
+            break;
+        }
+        default:
+        {
+            printf("Please check your loop again.\n");
+            break;
+        }
+        }
+
+    }
+
+    printf("Summation result : (FWD, BWD) : (%d, %d)\n", nNumSumFwdFlip, nNumSumBwdFlip);
+    printf("Probability : (FWD, BWD):(%.4lf, %.4lf)\n", (double)(nNumSumFwdFlip) / (NUM_FLIP), (double)(nNumSumBwdFlip) / (NUM_FLIP));
+
+    return 0;
+}
--- rock_paper_scissors/rock_paper_scissors/main.cpp
+++ rock_paper_scissors/rock_paper_scissors/main.cpp
@@ -1,114 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
 
+#define ROCK_PAPER_SCISSORS 3
+#define ROCK 0
+#define PAPER 1
+#define SCISSORS 2
+#define INITSTATUS 100
+#define END_COND 999
 
-#define rock_paper_scissors 3
-#define rock 0
-#define paper 1
-#define scissors 2
-#define initstatus 0
-#define end_cond 999
+#define PLAYER_WIN 11
+#define PLAYER_LOSE 12
+#define DRAW 13
 
-#define player_win 11
-#define player_lose 12
-#define draw 13
-
-
+// generating the seed number using the time clock information
 void GenRandSeed()
 {
-	srand((unsigned int)(time(NULL)));
-	return;
+    // generating the random number using the time seed
+    srand((unsigned int)(time(NULL)));
+    return;
 }
 
+// generating the random number
 unsigned int GenRandNum(unsigned int nRange)
 {
-	unsigned int nRes = 0;
-	unsigned int nNum = rand();
-	nRes = ((unsigned int)(nNum) % (nRange));
-	return nRes;
+    unsigned int nRes = 0;
+    unsigned int nNum = rand();
+    nRes = ((unsigned int)(nNum) % (nRange));
+    return nRes;
 }
 
+// calculating result
 unsigned int CalcRpsGameRes(unsigned int nPlayerRps, unsigned int nComRps)
 {
-	if (nPlayerRps == nComRps)
-	{
-		return draw;
-	}
+    // calculating draw condition
+    if (nPlayerRps == nComRps)
+        return DRAW;
 
-	if ((nPlayerRps == rock) && (nComRps == scissors))
-	{
-		return player_win;
-	}
-	else if ((nPlayerRps == scissors) && (nComRps == rock))
-	{
-		return player_lose;
-	}
-	else
-	{
-		if (nPlayerRps < nComRps)
-		{
-			return player_lose;
-		}
-		else
-		{
-			return player_win;
-		}
-	}
-
+    // rock and scissors condition
+    if ((nPlayerRps == ROCK) && (nComRps == SCISSORS))
+        return PLAYER_WIN;
+    else if ((nPlayerRps == SCISSORS) && (nComRps == ROCK))
+        return PLAYER_LOSE;
+    else
+    {
+        if (nPlayerRps < nComRps)
+            return PLAYER_LOSE;
+        else
+            return PLAYER_WIN;
+    }
 }
 
+// main function loop
 int main(void)
 {
-	int nRange = 0;
-	unsigned int nComRps = initstatus;
-	unsigned int nPlayerRps = initstatus;
+    // assigning seed
+    GenRandSeed();
 
-	while (1)
-	{
-		printf("Input (rock:0, paper:1, scissors:2)\n");
-		printf("If you want to end the loop, please input 999\n");
-		scanf_s("%d", &nPlayerRps);
+    // initializing variables
+    int nPlayerRps = INITSTATUS;
 
-		if (nPlayerRps == end_cond)
-		{
-			printf("End of the infinite while loop RPS game...\n");
-			break;
-		}
-		else if ((nPlayerRps < 0) || (nPlayerRps >= rock_paper_scissors))
-		{
-			printf("Check your input condition...\n\n");
-			continue;
-		}
+    // infinite loop
+    while (1)
+    {
+        // generating the player`s status
+        printf("Input Your Status(ROCK:0, PAPER:1, SCISSORS:2):\n");
+        printf("if you want to end the loop, please input 999\n");
+        scanf_s("%d", &nPlayerRps);
 
+        // exception #1, end condition
+        if (nPlayerRps == END_COND)
+        {
+            printf("end of the infinite while loop RPS game...\n");
+            break;
+        }
 
-		printf("(Player, Computer):(%d, %d)\n\n", nPlayerRps, nComRps);
-	}
-	unsigned int nComRps = GenRandNum(rock_paper_scissors);
-	unsigned int nGameRpsRes = CalcRpsGameRes(nPlayerRps, nComRps);
+        // exception #2, input condition
+        if ((nPlayerRps < 0) || (nPlayerRps >= ROCK_PAPER_SCISSORS))
+        {
+            printf("check your input condition..\n\n");
+            continue;
+        }
 
+        // generating the random number, ROCK/PAPER/SCISSORS
+        unsigned int nComRps = GenRandNum(ROCK_PAPER_SCISSORS);
 
-	switch (nGameRpsRes)
-	{
-	case player_win:
-	{
-		printf("Player win!\n\n");
-		break;
-	}
+        // calculating result
+        unsigned int nGameRpsRes = CalcRpsGameRes(nPlayerRps, nComRps);
 
-	case player_lose:
-	{
-		printf("Player lose...\n\n");
-		break;
-	}
-		
-	case draw:
-	{
-		printf("Draw!\n\n");
-		break;
-	}
-	}
+        // for debugging
+        printf("Status(Player, Computer):(%d, %d)\n", nPlayerRps, nComRps);
 
-	return 0;
+        // printing result
+        switch (nGameRpsRes)
+        {
+        case PLAYER_WIN:
+        {
+            printf("Player Win!, Computer Lose..\n\n");
+            break;
+        }
+        case PLAYER_LOSE:
+        {
+            printf("Player Lose...Computer Win!\n\n");
+            break;
+        }
+        case DRAW:
+        {
+            printf("DRAW!!\n\n");
+            break;
+        }
+        default:
+        {
+            printf("please check your result..\n\n");
+            break;
+        }
+        }
+    }
+
+    return 0;
 }
