9eff9babb1cd65c3823e49668673740104853b14
Kim-Sehyeong <threebros00@gmail.com>
2024-12-11 11:25:41 +0900

    12.11
    12.11

--- report#4_array_ten_integers/report#4_array_ten_integers/main.cpp
+++ report#4_array_ten_integers/report#4_array_ten_integers/main.cpp
@@ -35,7 +35,7 @@ int sum(int* p_arr, int arr_size) // 배열 arr의 각 요소의 합을 구하는 함수
     return res;
 }
 
-void dev(int* p_arr, int avg, int arr_size, int* p_arr_dev) // 배열 arr의 각 요소와 평균의 차를 구해 배열 arr_dev에 대입
+void dev(int* p_arr, double avg, int arr_size, double* p_arr_dev) // 배열 arr의 각 요소와 평균의 차를 구해 배열 arr_dev에 대입
 {
     for (int i = 0; i < arr_size; i++)
     {
@@ -43,9 +43,9 @@ void dev(int* p_arr, int avg, int arr_size, int* p_arr_dev) // 배열 arr의 각 요�
     }
 }
 
-int var(int avg, int arr_size, int* p_arr_dev) // 배열 arr_dev의 각 요소의 제곱을 모두 더해 배열 크기로 나눔
+double var(int arr_size, double* p_arr_dev) // 배열 arr_dev의 각 요소의 제곱을 모두 더해 배열 크기로 나눔
 {
-    int res = 0;
+    double res = 0.0;
     for (int i = 0; i < arr_size; i++)
     {
         res += (*(p_arr_dev + i)) * (*(p_arr_dev + i));
@@ -53,7 +53,7 @@ int var(int avg, int arr_size, int* p_arr_dev) // 배열 arr_dev의 각 요소의 제곱�
     return res / arr_size;
 }
 
-int std_dev(int var) //표준편차
+double std_dev(double var) //표준편차
 {
     return sqrt(var);
 }
@@ -64,11 +64,12 @@ int main(void)
     GenRandSeed(); //난수 함수 호출
 
     int arr[10] = { };
-    int arr_dev[10] = { };
+    double arr_dev[10] = { };
     int* p_arr = arr;
-    int* p_arr_dev = arr_dev;
+    double* p_arr_dev = arr_dev;
     const int arr_size = sizeof(arr) / sizeof(arr[0]);
-    int sum_res, avg, var_res, std_dev_res = 0;
+    int sum_res = 0;
+    double avg, var_res, std_dev_res = 0.0;
 
     // 배열 arr의 각 요소로 난수를 대입
     for (int i = 0; i < arr_size; i++)
@@ -80,7 +81,7 @@ int main(void)
     sum_res = sum(p_arr, arr_size); // 합
     avg = sum_res / arr_size; // 평균
     dev(p_arr, avg, arr_size, p_arr_dev); // 편차
-    var_res = var(avg, arr_size, p_arr_dev); // 분산
+    var_res = var(arr_size, p_arr_dev); // 분산
     std_dev_res = std_dev(var_res); // 표준 편차
 
     // 난수로 구성된 배열 출력
@@ -91,6 +92,6 @@ int main(void)
     }
     printf("\n");
 
-    printf("sum, variable, standard deviation : (%d, %d, %d)", sum_res, var_res, std_dev_res);
+    printf("sum, variable, standard deviation : (%d, %.2lf, %.2lf)", sum_res, var_res, std_dev_res);
     return 0;
 }
